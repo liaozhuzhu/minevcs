a5d24618a0fb530a0afa545f9bfffd5878ef3c32
Liao Zhu <liao.zhu.cs@gmail.com>
2025-04-18 12:19:39 -0500

    upload folder

--- quickstart/quickstart.go
+++ quickstart/quickstart.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -81,6 +82,104 @@ func UploadFile(ctx context.Context, srv *drive.Service, file *os.File) (*drive.
 	return res, nil
 }
 
+func findFolder(srv *drive.Service, name string, parentId string) (string, error) {
+	query := fmt.Sprintf("name = '%s' and mimeType = 'application/vnd.google-apps.folder' and trashed = false and 'root' in parents", name)
+	if parentId != "" {
+		query += fmt.Sprintf(" and '%s' in parents", parentId)
+	}
+
+	res, err := srv.Files.List().
+		Q(query).
+		Fields("files(id, name)").
+		Do()
+	if err != nil {
+		return "", err
+	}
+	if len(res.Files) > 0 {
+		return res.Files[0].Id, nil // return first match
+	}
+	return "", nil // not found
+}
+
+func uploadFolder(srv *drive.Service, name string, parentId string) (string, error) {
+	existingId, err := findFolder(srv, name, parentId)
+	if err != nil {
+		return "", err
+	}
+	if existingId != "" {
+		println("Folder already exists:", existingId)
+		// folder exists delete it so we can replace it
+		err = srv.Files.Delete(existingId).Do()
+		if err != nil {
+			return "", fmt.Errorf("unable to delete existing folder: %v", err)
+		}
+	}
+	folder := &drive.File{
+		Name:     name,
+		MimeType: "application/vnd.google-apps.folder",
+	}
+	if parentId != "" {
+		folder.Parents = []string{parentId}
+	}
+	created, err := srv.Files.Create(folder).Fields("id").Do()
+	if err != nil {
+		return "", fmt.Errorf("unable to create folder: %v", err)
+	}
+	fmt.Println("Created folder:", name)
+	return created.Id, nil
+}
+
+func uploadDirectoryToDrive(srv *drive.Service, localPath string, parentDriveID string) error {
+	return filepath.Walk(localPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip the base directory (already created as parentDriveID)
+		if path == localPath {
+			return nil
+		}
+
+		relPath, _ := filepath.Rel(localPath, path)
+
+		if info.IsDir() {
+			// Create Drive folder
+			folder := &drive.File{
+				Name:     info.Name(),
+				MimeType: "application/vnd.google-apps.folder",
+				Parents:  []string{parentDriveID},
+			}
+			created, err := srv.Files.Create(folder).Fields("id").Do()
+			if err != nil {
+				return fmt.Errorf("could not create folder %s: %w", relPath, err)
+			}
+			// Recurse into subdirectory
+			return uploadDirectoryToDrive(srv, path, created.Id)
+		} else {
+			// Upload file
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			driveFile := &drive.File{
+				Name:    info.Name(),
+				Parents: []string{parentDriveID},
+			}
+			_, err = srv.Files.Create(driveFile).
+				Media(f).
+				Fields("id").
+				Do()
+			if err != nil {
+				return fmt.Errorf("upload failed for %s: %w", relPath, err)
+			}
+			fmt.Println("Uploaded:", relPath)
+		}
+		return nil
+	})
+}
+
 func main() {
 	ctx := context.Background()
 	b, err := os.ReadFile("credentials.json")
@@ -88,7 +187,6 @@ func main() {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
-	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, drive.DriveScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
@@ -100,24 +198,31 @@ func main() {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
-	shortFile, err := os.Open("test.txt")
-	outFile, err := UploadFile(ctx, srv, shortFile)
-	if err != nil {
-		log.Fatalf("Unable to upload file: %v", err)
-	}
-	fmt.Printf("File '%s' uploaded with ID: %s\n", outFile.Name, outFile.Id)
+	// shortFile, err := os.Open("test.txt")
+	// outFile, err := UploadFile(ctx, srv, shortFile)
+	// if err != nil {
+	// 	log.Fatalf("Unable to upload file: %v", err)
+	// }
+	// fmt.Printf("File '%s' uploaded with ID: %s\n", outFile.Name, outFile.Id)
 
-	r, err := srv.Files.List().PageSize(10).
-		Fields("nextPageToken, files(id, name)").Do()
+	// r, err := srv.Files.List().PageSize(10).
+	// 	Fields("nextPageToken, files(id, name)").Do()
+	// if err != nil {
+	// 	log.Fatalf("Unable to retrieve files: %v", err)
+	// }
+	// fmt.Println("Files:")
+	// if len(r.Files) == 0 {
+	// 	fmt.Println("No files found.")
+	// } else {
+	// 	for _, i := range r.Files {
+	// 		fmt.Printf("%s (%s)\n", i.Name, i.Id)
+	// 	}
+	// }
+
+	// Define the parent folder in Drive
+	folderId, err := uploadFolder(srv, "test", "")
 	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
+		log.Fatalf("Unable to create folder: %v", err)
 	}
-	fmt.Println("Files:")
-	if len(r.Files) == 0 {
-		fmt.Println("No files found.")
-	} else {
-		for _, i := range r.Files {
-			fmt.Printf("%s (%s)\n", i.Name, i.Id)
-		}
-	}
+	println("Upload folder with ID:", folderId)
 }
