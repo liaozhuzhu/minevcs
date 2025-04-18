81fa414f62867fef81e6fd9e4ad5084d7a11289e
Liao Zhu <liao.zhu.cs@gmail.com>
2025-04-18 13:21:12 -0500

    clean up

--- app.go
+++ app.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/shirou/gopsutil/v3/process"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/drive/v3"
-	"google.golang.org/api/googleapi"
-	"google.golang.org/api/option"
 )
 
 type ProgressReader struct {
@@ -40,16 +33,6 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-func (a *App) getClient(config *oauth2.Config) *http.Client {
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(a.ctx, tok)
-}
-
 func (a *App) CheckMinecraftRunning(expectedPath string) (bool, error) {
 	procs, err := process.Processes()
 	if err != nil {
@@ -67,54 +50,8 @@ func (a *App) CheckMinecraftRunning(expectedPath string) (bool, error) {
 	return false, nil
 }
 
-func (a *App) UploadFile(localFilePath, driveFolder string) (string, error) {
-	ctx := a.ctx
-	b, err := os.ReadFile("credentials.json")
-	if err != nil {
-		return "", fmt.Errorf("unable to read client secret file: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(b, drive.DriveScope)
-	if err != nil {
-		return "", fmt.Errorf("unable to parse config: %v", err)
-	}
-	client := a.getClient(config)
-	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		return "", fmt.Errorf("unable to create Drive client: %v", err)
-	}
-
-	file, err := os.Open(localFilePath)
-	if err != nil {
-		return "", fmt.Errorf("unable to open file: %v", err)
-	}
-	defer file.Close()
-
-	driveFile := &drive.File{
-		Name: filepath.Base(localFilePath),
-	}
-	if driveFolder != "" {
-		driveFile.Parents = []string{driveFolder}
-	}
-
-	fmt.Println("Uploading:", localFilePath)
-	res, err := srv.Files.Create(driveFile).
-		Media(file, googleapi.ChunkSize(1024*1024)).
-		Fields("id, name").
-		Do()
-	if err != nil {
-		return "", fmt.Errorf("Drive upload failed: %v", err)
-	}
-
-	fmt.Printf("File '%s' uploaded with ID: %s\n", res.Name, res.Id)
-	return res.Id, nil
-}
-
 func (a *App) ListFiles(filePath string) ([]string, error) {
 	_, _ = a.CheckMinecraftRunning("/Applications/Minecraft.app/Contents/MacOS/launcher")
-	if _, err := a.UploadFile("test.txt", ""); err != nil {
-		return nil, err
-	}
 
 	home, err := os.UserHomeDir()
 	if err != nil {
