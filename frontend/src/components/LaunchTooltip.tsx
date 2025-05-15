a945d29eec38b1c6b0c950b33eed93b026cda50a
Liao Zhu <liao.zhu.cs@gmail.com>
2025-05-15 02:37:10 -0500

    tooltip update ✍️

--- frontend/src/components/LaunchTooltip.tsx
+++ frontend/src/components/LaunchTooltip.tsx
@@ -1,11 +1,13 @@
 const LaunchTooltip = () => {
     return (
         <div className="absolute top-6 left-0 bg-white text-black p-2 rounded-md shadow-md z-10">
-                      <p className="text-xs">You can find your Launcher exe path in your finder.</p>
+                      <p className="text-xs">You can find your Launcher exe path in your find / file explorer</p>
                       <ol className='list-decimal list-inside'>
-                        <li className="text-xs">Find your Minecraft App</li>
-                        <li className="text-xs">Right click and click "Show Package Contents"</li>
-                        <li className="text-xs">Inside "Contents" find your OS folder and finally copy paste the file path to the <pre>launcher.exe</pre></li>
+                        <li className="text-xs">Find your Minecraft Launcher App (most likely in Applications)</li>
+                        <li className="text-xs">For MacOS, right click and click "Show Package Contents"</li>
+                        <li className="text-xs">For Windows, your exe might be contained wihtin "Minecraft Launcher"</li>
+                        <li className="text-xs">Go into "Contents" and if on MacOS go into "MacOS"</li>
+                        <li className="text-xs">Finally copy paste the file path to the <pre>launcher(.exe)</pre></li>
                         <li className="text-xs">For example: <pre>/Application/Minecraft.app/Contents/MacOS/launcher</pre></li>
                       </ol>
                       </div>
